38c89ac7df25578ba8d7cd226bb6984d78ff2c56
Sainekt <xcision@ya.ru>
2024-09-24 15:33:09 +0300

    admin is done

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 
+
 from .models import (
     Tag,
     Ingredient,
@@ -16,6 +17,11 @@ admin.site.empty_value_display = 'Не задано'
 User = get_user_model()
 
 
+class TagsRecipesInline(admin.TabularInline):
+    model = TagsRecipes
+    extra = 1
+
+
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'slug')
@@ -25,50 +31,52 @@ class TagAdmin(admin.ModelAdmin):
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'measurement_unit')
-    search_fields = ('name', 'measurement_unit')
-    list_editable = ('name', 'measurement_unit')
-    readonly_fields = ('id',)
+    list_display = ('name', 'measurement_unit')
+    search_fields = ('name',)
+    list_display_links = ('name',)
 
 
 @admin.register(IngredientsRecipes)
-class IngredientsRecipes(admin.ModelAdmin):
+class IngredientsRecipesAdmin(admin.ModelAdmin):
     list_display = ('ingredient', 'recipe')
     list_display_links = ('recipe',)
-    search_fields = ('ingredient', 'recipe')
+    search_fields = ('ingredient__name', 'recipe__name')
     list_editable = ('ingredient',)
     prefetch_related = ('recipe_ingredients',)
 
 
 @admin.register(Recipe)
-class Recipe(admin.ModelAdmin):
-    list_display = (
-        'id', 'image', 'name', 'text',
-        'author', 'short_link'
-    )
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'author__username', 'favorite_count')
+    search_fields = ('author__username', 'name',)
+    list_display_links = ('id', 'name')
+    list_filter = ('tags__name',)
+    inlines = [TagsRecipesInline]
 
-    search_fields = ('text', 'author__username', 'name__name')
-    list_editable = ('text', 'author', 'name')
+    def favorite_count(self, obj):
+        return obj.favorite_recipes.count()
+    favorite_count.admin_order_field = 'favorite_recipes'
+    favorite_count.short_description = 'Добавлений в избранное'
 
 
 @admin.register(TagsRecipes)
-class TagsRecipes(admin.ModelAdmin):
+class TagsRecipesAdmin(admin.ModelAdmin):
     list_display = ('id', 'tag', 'recipe')
-    search_fields = ('tag', 'recipe')
-    prefetch_related = ('recipe_tags',)
+    search_fields = ('tag__name', 'recipe__name')
 
 
 @admin.register(ShoppingCart)
-class ShoppingCart(admin.ModelAdmin):
+class ShoppingCartAdmin(admin.ModelAdmin):
     list_display = (
         'user', 'recipe'
     )
-    search_fields = ('user__username', 'recipe')
+    search_fields = ('user__username', 'recipe__name')
 
 
 @admin.register(FavoriteRecipes)
-class FavoriteRecipes(admin.ModelAdmin):
+class FavoriteRecipesAdmin(admin.ModelAdmin):
     list_display = (
-        'user', 'recipe'
+        'user', 'recipe',
     )
-    search_fields = ('user__username', 'recipe')
+    list_display_links = ('user', 'recipe',)
+    search_fields = ('user__username', 'recipe__name')
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Subscriber
+
+
+@admin.register(User)
+class User(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name')
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    list_display_links = ('username', 'last_name', 'first_name')
+
+
+@admin.register(Subscriber)
+class Subscriber(admin.ModelAdmin):
+    list_display = ('id', 'user', 'subscriber')
+    search_fields = ('user', 'subscriber')
+    list_editable = ('user', 'subscriber')
