ceb24f9d401ea7331ccb67d848a0cac333191cda
Sainekt <xcision@ya.ru>
2024-09-27 12:42:51 +0300

    add UserAdmin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import User, Subscriber
 
-
-@admin.register(User)
-class User(admin.ModelAdmin):
-    list_display = ('id', 'username', 'email', 'first_name', 'last_name')
-    search_fields = ('email', 'username', 'first_name', 'last_name')
-    list_display_links = ('username', 'last_name', 'first_name')
+admin.site.register(User, UserAdmin)
 
 
 @admin.register(Subscriber)
