b673c6215c9587911511432b4aa50a16be2c640c
Роман Желонкин <xcision@ya.ru>
2024-09-29 11:56:51 +0300

    about and techologies unblock

--- frontend/src/pages/about/index.js
+++ frontend/src/pages/about/index.js
@@ -38,10 +38,10 @@ const About = ({ updateOrders, orders }) => {
           </h2>
           <div className={styles.text}>
             <p className={styles.textItem}>
-              Код проекта находится тут - <a href="#" className={styles.textLink}>Github</a>
+              Код проекта находится тут - <a href="https://github.com/Sainekt/foodgram" className={styles.textLink}>Github</a>
             </p>
             <p className={styles.textItem}>
-              Автор проекта: <a href="#" className={styles.textLink}>Имя Автора</a>
+              Автор проекта: <a href="https://vk.com/sainekt" className={styles.textLink}>Желонкин Роман</a>
             </p>
           </div>
         </aside>
